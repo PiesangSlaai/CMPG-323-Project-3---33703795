2142032fd57bc01746183ac32e287dc8dfa1d111
PiesangSlaai <ziankara0@gmail.com>
2023-09-21 12:07:52 +0200

    Created Generic Repository and Interface

---
+++ SuperStore P3/Interfaces/IGenericRepositoryInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> GetByConditionAsync(Expression<Func<TEntity, bool>> condition);
+        Task<TEntity> GetByIdAsync(object id);
+        Task CreateAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(object id);
+        bool Exists(Expression<Func<TEntity, bool>> condition);
+    }
+}
---
+++ SuperStore P3/Repositories/GenericRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repositories
+{
+    public class GenericRepository<TEntity> where TEntity : class
+    {
+        private readonly SuperStoreContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericRepository(SuperStoreContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetByConditionAsync(Expression<Func<TEntity, bool>> condition)
+        {
+            return await _dbSet.Where(condition).ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(object id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task CreateAsync(TEntity entity)
+        {
+            _dbSet.Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(object id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public bool Exists(Expression<Func<TEntity, bool>> condition)
+        {
+            return _dbSet.Any(condition);
+        }
+    }
+}
